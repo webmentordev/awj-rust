fb78532dafc2454e0fc03803a999d7458ae71cb0
Muhammad Ahmer Tahir <82140155+webmentordev@users.noreply.github.com>
2024-04-03 03:27:22 +0500

    Staff member removed

--- pages/staff.vue
+++ pages/staff.vue
@@ -3,7 +3,7 @@
         <div class="absolute top-0 left-0 h-full w-full bg-dark/60 backdrop-blur-sm"></div>
         <div class="relative z-10 max-w-2xl m-auto pt-[140px] px-4">
             <h1 class="text-5xl text-white text-center 480px:text-4xl 550px:mb-3">Our Staff Members</h1>
-            <div class="grid gap-2 grid-cols-2 justify-center mt-5 mb-5 550px:grid-cols-1 550px:max-w-[300px] 550px:m-auto">
+            <div class="grid gap-2 grid-cols-1 justify-center mt-5 mb-5 m-auto max-w-[320px]">
                 <div class=" text-white w-full">
                     <div class="bg-black p-6 rounded-lg relative group">
                         <img class="mb-5 rounded-md group-hover:scale-125 transition-all w-full" src="/chadder.png">
@@ -13,19 +13,6 @@
                             <div class="flex text-md items-center justify-center">
                                 <Icon class="mr-2" name="logos:discord-icon" />
                                 <span class="text-sm">awjchedder</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class=" text-white w-full">
-                    <div class="bg-black p-6 rounded-lg relative group">
-                        <img class="mb-5 rounded-md group-hover:scale-125 transition-all w-full" src="/awj_owner.png">
-                        <div class="info">
-                            <h2 class="text-center text-xl mb-2">AWJ IZYOUMADD</h2>
-                            <span class="rounded-sm font-semibold p-2 text-sm px-3 bg-rust inline-block mb-3 w-full text-center">Owner (Console Servers)</span>
-                            <div class="flex text-md items-center justify-center">
-                                <Icon class="mr-2" name="logos:discord-icon" />
-                                <span class="text-sm">awjizyoumaddd</span>
                             </div>
                         </div>
                     </div>
