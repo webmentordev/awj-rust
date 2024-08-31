f2c64c0c99658d9b65cbdf307fc6761896962bcd
Muhammad Ahmer Tahir <82140155+webmentordev@users.noreply.github.com>
2024-08-31 22:06:17 +0500

    Ip change

--- components/PC.vue
+++ components/PC.vue
@@ -1,5 +1,7 @@
 <template>
-    <section class="fixed top-0 left-0 px-4 bg-dark bg-opacity-70 flex items-center justify-center z-50 w-full h-full backdrop-blur-md" @click.self="$emit('pc-toggle')">
+    <section
+        class="fixed top-0 left-0 px-4 bg-dark bg-opacity-70 flex items-center justify-center z-50 w-full h-full backdrop-blur-md"
+        @click.self="$emit('pc-toggle')">
         <div class="max-w-lg w-full p-6 bg-dark-100 rounded-lg text-white">
             <div class="text-center">
                 <h1 class="text-3xl mb-2">AWJ PC Rust Servers</h1>
@@ -7,11 +9,11 @@
             </div>
             <!-- <h2 class="text-2xl mb-1">2X PVE</h2>
             <p class="p-3 rounded-lg bg-dark mb-3">
-                connect 156.236.84.209:28015
+                connect 185.206.151.2:28015
             </p> -->
             <h2 class="text-2xl mb-1">2X PVP</h2>
             <p class="p-3 rounded-lg bg-dark mb-3">
-                connect 156.236.84.197:28040
+                connect 185.206.151.2:28015
             </p>
         </div>
     </section>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,19 @@
 <template>
     <section>
-        <div class="w-full bg-center bg-cover min-h-screen relative flex items-center justify-center" style="background-image: url('/home.webp');">
+        <div class="w-full bg-center bg-cover min-h-screen relative flex items-center justify-center"
+            style="background-image: url('/home.webp');">
             <div class="absolute top-0 left-0 h-full w-full bg-dark/60 backdrop-blur-sm"></div>
             <div class="relative z-10 px-4">
-                <img src="/logo.webp" class="m-auto mb-6 w-full max-w-[400px] 480px:max-w-[250px] rounded-full" alt="Exile Logo">
+                <img src="/logo.webp" class="m-auto mb-6 w-full max-w-[400px] 480px:max-w-[250px] rounded-full"
+                    alt="Exile Logo">
                 <h1 class="text-center text-white mb-3 text-6xl">AWJ Rust Servers</h1>
                 <ul class="m-auto flex items-center justify-center">
-                    <!-- <NuxtLink class="mr-2 py-3 rounded hover:text-black hover:bg-main transition-all px-6 bg-dark link text-white text-xl inline-block" to="steam://connect/156.236.84.209:28015"><Icon name="bi:steam" class="text-2xl mr-1" /> 2X PVE Connect</NuxtLink> -->
-                    <NuxtLink class="mr-2 py-3 rounded hover:text-black hover:bg-main transition-all px-6 bg-dark link text-white text-xl inline-block" to="steam://connect/156.236.84.197:28040"><Icon name="bi:steam" class="text-2xl mr-1" /> 2X PVP Connect</NuxtLink>
+                    <!-- <NuxtLink class="mr-2 py-3 rounded hover:text-black hover:bg-main transition-all px-6 bg-dark link text-white text-xl inline-block" to="steam://connect/185.206.151.2:28015"><Icon name="bi:steam" class="text-2xl mr-1" /> 2X PVE Connect</NuxtLink> -->
+                    <NuxtLink
+                        class="mr-2 py-3 rounded hover:text-black hover:bg-main transition-all px-6 bg-dark link text-white text-xl inline-block"
+                        to="steam://connect/185.206.151.2:28015">
+                        <Icon name="bi:steam" class="text-2xl mr-1" /> 2X PVP Connect
+                    </NuxtLink>
                 </ul>
                 <!-- <div class="flex w-full items-center justify-center mt-3">
                     <span class="m-auto cursor-pointer py-3 rounded hover:text-black hover:bg-main transition-all px-6 bg-dark link text-white text-xl inline-block" @click="console_toggle = !console_toggle"><Icon name="game-icons:console-controller" class="text-2xl mr-1" /> Console</span>
@@ -17,7 +23,8 @@
                 <div class="flex items-center">
                     <h4 class="text-white mr-3 text-2xl link">Read More</h4>
                     <div class="rounded-full bg-main p-2">
-                        <img src="https://api.iconify.design/ic:outline-keyboard-double-arrow-down.svg" alt="Arrow Down" width="30">
+                        <img src="https://api.iconify.design/ic:outline-keyboard-double-arrow-down.svg" alt="Arrow Down"
+                            width="30">
                     </div>
                 </div>
             </div>
@@ -25,7 +32,9 @@
 
         <div class="w-full bg-dark py-12 px-4">
             <h2 class="text-main text-center mb-4 text-5xl">Our Server Configuration</h2>
-            <p class="text-center text-white mb-3">Here is the configuration that we use for our server. <br> Feel free to read all. It may come handy because we have so much <br> to offer. Commands are also mentioned below the configuration</p>
+            <p class="text-center text-white mb-3">Here is the configuration that we use for our server. <br> Feel free
+                to read all. It may come handy because we have so much <br> to offer. Commands are also mentioned below
+                the configuration</p>
             <p v-if="pending" class="text-center link text-white">Loading...</p>
             <div v-else class="max-w-2xl m-autopx-4 text-white m-auto config">
                 <!-- <div class="flex items-center mb-3">
@@ -45,12 +54,12 @@
 </template>
 
 <script setup>
-    const configs = ref(null);
-    const server = ref("pvp");
-    
-    let { data, pending } = await useFetch('/api/config');
-    configs.value = data.value;
-    
-    const console_toggle = ref(false);
+const configs = ref(null);
+const server = ref("pvp");
+
+let { data, pending } = await useFetch('/api/config');
+configs.value = data.value;
+
+const console_toggle = ref(false);
 
 </script>
